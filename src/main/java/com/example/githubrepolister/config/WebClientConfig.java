974fb9cb70cbba87a4c03071aee0e1c63f58ef6b
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-07-19 19:23:02 +0300

    replaced constant value to Constants class

--- src/main/java/com/example/githubrepolister/config/WebClientConfig.java
+++ src/main/java/com/example/githubrepolister/config/WebClientConfig.java
@@ -1,13 +1,13 @@
 package com.example.githubrepolister.config;
 
+import static com.example.githubrepolister.constants.Constants.GITHUB_API_BASE_URL;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-    public static final String GITHUB_API_BASE_URL = "https://api.github.com";
-
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder()
--- src/main/java/com/example/githubrepolister/constants/Constants.java
+++ src/main/java/com/example/githubrepolister/constants/Constants.java
@@ -2,6 +2,7 @@ package com.example.githubrepolister.constants;
 
 public class Constants {
     public static final String APPLICATION_JSON = "application/json";
+    public static final String GITHUB_API_BASE_URL = "https://api.github.com";
     public static final String USER_REPOS_URI = "/users/{username}/repos";
     public static final String USER_REPOS_BRANCHES_URI = "/repos/{username}/{repo}/branches";
 
